22f2b6e339841ce8b5c307f2320ff87480a83dd7
Ayden Hoyll <aakazakov97@gmail.com>
2023-03-29 20:50:21 -0500

    v1

--- pyMain.py
+++ pyMain.py
@@ -2,8 +2,8 @@ import re
 import nltk
 import libs
 # open txt file and normalize it (i.e., to delete all punctuation and numbers)
-libs.count_sentences('WhiteNoiseDeLillo.txt')
-libs.find_attributive_adjectives('WhiteNoiseDeLillo.txt')
-libs.find_prepositional_phrases('WhiteNoiseDeLillo.txt')
-libs.find_vb_that_pairs('WhiteNoiseDeLillo.txt')
-libs.find_verb_wh('WhiteNoiseDeLillo.txt')
+libs.count_sentences('text.txt')
+libs.find_attributive_adjectives('text.txt')
+libs.find_prepositional_phrases('text.txt')
+libs.find_vb_that_pairs('text.txt')
+libs.find_verb_wh('text.txt')
