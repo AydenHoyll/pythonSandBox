39e0a8346d48f11a9df06330d8813bc2cca4bf0d
Ayden Hoyll <aakazakov97@gmail.com>
2023-04-19 12:12:55 -0500

    'added passive voice'

--- libs.py
+++ libs.py
@@ -1,7 +1,10 @@
-import re
 import nltk
 
+
+
+
 def normalize_tokenize_tag(file_path):
+    """normalize texts inputs and applies pos"""
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
     # turn each word into individual unit of an array/list and normalizes text
@@ -10,6 +13,7 @@ def normalize_tokenize_tag(file_path):
     return tagged
 
 def find_verb_wh(file_path):
+    """look for verb + wh clause"""
     tagged = normalize_tokenize_tag(file_path)
     verb_wh = []
     for i in range(len(tagged) - 1):
@@ -18,18 +22,22 @@ def find_verb_wh(file_path):
 
         if tag1.startswith('VB') and tag2.startswith('W'):
             verb_wh.append(word1 + ' ' + word2)
-    print('number of verb+wh clauses:', len(verb_wh))
+    print('Number of verb+wh clauses:', len(verb_wh))
     return verb_wh
 
 def count_sentences(file_path):
+    """get number of sentences"""
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
     sentences = nltk.sent_tokenize(text)
     num_sentences = len(sentences)
+    tokens = [token for token in nltk.word_tokenize(text) if token.isalpha()]
+    print('Number of words:', len(tokens))
     print('Number of sentences:', num_sentences)
     return num_sentences
 
 def find_vb_that_pairs(file_path):
+    """get verb +that-clause"""
     tagged = normalize_tokenize_tag(file_path)
 
     vb_that = []
@@ -44,7 +52,7 @@ def find_vb_that_pairs(file_path):
     return vb_that
 
 def find_attributive_adjectives(file_path):
-    
+    """find all attr adjectives"""
     tagged = normalize_tokenize_tag(file_path)
 
     attrib_adjs = []
@@ -52,10 +60,11 @@ def find_attributive_adjectives(file_path):
     for i in range(1, len(tagged)):
         if tagged[i][1] == 'JJ' and tagged[i-1][1] in ['NN', 'NNS', 'NNP', 'NNPS']:
             attrib_adjs.append(tagged[i][0])
-    print('number of attr adjectives:', len(attrib_adjs))
+    print('Number of attr adjectives:', len(attrib_adjs))
     return attrib_adjs
 
 def find_prepositional_phrases(file_path):
+    """find all PP"""
     # Preposition pattern
     preposition_pattern = 'IN|TO|OF|WITH|FOR|ON|AT|FROM|BY|ABOUT'
 
@@ -74,5 +83,50 @@ def find_prepositional_phrases(file_path):
     prepositional_phrases = []
     for subtree in parsed.subtrees(filter=lambda t: t.label() == 'PP'):
         prepositional_phrases.append(' '.join(word for word, tag in subtree.leaves()))
-    print('number of prep phrases:', len(prepositional_phrases))
+    print('Number of prep phrases:', len(prepositional_phrases))
     return prepositional_phrases
+
+def isPassive(sentence):
+    """checks if the sentence contains passive voice"""
+    beforms = ['am', 'is', 'are', 'been', 'was', 'were', 'be', 'being']              
+    aux = ['do', 'did', 'does', 'have', 'has', 'had']                                  
+    words = nltk.word_tokenize(sentence)
+    tokens = nltk.pos_tag(words)
+    tags = [i[1] for i in tokens]
+    if tags.count('VBN') == 0:                                                         
+        return False
+    elif tags.count('VBN') == 1 and 'been' in words:                                   
+        return False
+    else:
+        pos = [i for i in range(len(tags)) if tags[i] == 'VBN' and words[i] != 'been']
+        for end in pos:
+            chunk = tags[:end]
+            start = 0
+            for i in range(len(chunk), 0, -1):
+                last = chunk.pop()
+                if last == 'NN' or last == 'PRP':
+                    start = i
+                    break
+            sentchunk = words[start:end]
+            tagschunk = tags[start:end]
+            verbspos = [i for i in range(len(tagschunk)) if tagschunk[i].startswith('V')] 
+            if verbspos != []:                                                            
+                for i in verbspos:
+                    if sentchunk[i].lower() not in beforms and sentchunk[i].lower() not in aux: 
+                        break
+                else:
+                    return True
+    return False
+
+def passiveVoiceFinder(file_path):
+    """finds all the passive voice"""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    sents = nltk.sent_tokenize(text)
+    passiveVoiceArr = []
+    for sent in sents:
+        if isPassive(sent):
+            passiveVoiceArr.append(sent)
+        # print(sent + '--> %s' % isPassive(sent))
+    print(file_path,'pvoice:', len(passiveVoiceArr))
+    return passiveVoiceArr
--- pyMain.py
+++ pyMain.py
@@ -1,9 +1,39 @@
-import re
-import nltk
 import libs
-# open txt file and normalize it (i.e., to delete all punctuation and numbers)
-libs.count_sentences('text.txt')
-libs.find_attributive_adjectives('text.txt')
-libs.find_prepositional_phrases('text.txt')
-libs.find_vb_that_pairs('text.txt')
-libs.find_verb_wh('text.txt')
+
+# libs.find_attributive_adjectives('Jim_Thompson_killer_inside.txt')
+# libs.find_prepositional_phrases('Jim_Thompson_killer_inside.txt')
+# libs.find_vb_that_pairs('Jim_Thompson_killer_inside.txt')
+# libs.find_verb_wh('Jim_Thompson_killer_inside.txt')
+# print('/n')
+# libs.find_attributive_adjectives('A_Kiss_Before_Dying-Ira_Levin.txt')
+# libs.find_prepositional_phrases('A_Kiss_Before_Dying-Ira_Levin.txt')
+# libs.find_vb_that_pairs('A_Kiss_Before_Dying-Ira_Levin.txt')
+# libs.find_verb_wh('A_Kiss_Before_Dying-Ira_Levin.txt')
+# print('/n')
+# libs.find_attributive_adjectives('Killer_s_Wedge_McBain.txt')
+# libs.find_prepositional_phrases('Killer_s_Wedge_McBain.txt')
+# libs.find_vb_that_pairs('Killer_s_Wedge_McBain.txt')
+# libs.find_verb_wh('Killer_s_Wedge_McBain.txt')
+# print('/n')
+# libs.find_attributive_adjectives('The_Galton_Case_MacDonald.txt')
+# libs.find_prepositional_phrases('The_Galton_Case_MacDonald.txt')
+# libs.find_vb_that_pairs('The_Galton_Case_MacDonald.txt')
+# libs.find_verb_wh('The_Galton_Case_MacDonald.txt')
+libs.passiveVoiceFinder('mccarthy_groves.txt')
+print('/n')
+libs.passiveVoiceFinder('pictures_from_an_institution_Jarell.txt')
+print('/n')
+libs.passiveVoiceFinder('pnin_nabokov_-vladimir.txt')
+print('/n')
+libs.passiveVoiceFinder('The_Galton_Case_MacDonald.txt')
+print('/n')
+
+libs.passiveVoiceFinder('Jim_Thompson_killer_inside.txt')
+print('/n')
+
+libs.passiveVoiceFinder('Killer_s_Wedge_McBain.txt')
+print('/n')
+libs.passiveVoiceFinder('A_Kiss_Before_Dying-Ira_Levin.txt')
+
+
+
